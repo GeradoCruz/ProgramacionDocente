80a08311b4c74733c57082009b4f0a306d42b80d
Gerardo Cruz <gerardhdez123@gmail.com>
2026-07-06 14:51:26 -0600

    Agregar archivos de proyecto.

---
+++ ProgramacionDocente/clsConexion.cs
@@ -0,0 +1,49 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramacionDocente
+{
+    internal class clsConexion
+    {
+        private string host = "189.240.192.140";
+        private string bd = "bdescuelagbequipo1";
+        private string usuario = "gbEquipo1";
+        private string password = "grupo1";
+        private string puerto = "3306";
+
+        private string cadenaConexion => $"server={host}; database={bd}; user={usuario}; password={password}; port={puerto};";
+        public MySqlConnection AbrirConexion()
+        {
+            var conexion = new MySqlConnection(cadenaConexion);
+            try
+            {
+                conexion.Open();
+                return conexion;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error al intentar conectarse a la base de datos :" + ex.Message, ex);
+            }
+        }
+        public void CerrarConexion(MySqlConnection conexion)
+        {
+            try
+            {
+                if (conexion != null && conexion.State != ConnectionState.Closed)
+                {
+                    conexion.Close();
+                    conexion.Dispose();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error al intentar cerrar la conexión a la base de datos :" + ex.Message, ex);
+            }
+        }
+    }
+}
